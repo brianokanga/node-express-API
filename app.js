a8bc4f8f7e49637020baba8983b4825fc25a4484
brianokanga <briannokanga@gmail.com>
2020-09-05 10:47:50 +0300

    added the get tours route and read data

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-const express = require ('express');
+const fs = require('fs');
 
-const app = express ();
+const express = require('express');
 
-// routes
-app.get ('/', (req, res) => {
-  res
-    .status (200)
-    .json ({mesage: 'Hello from the server.', app: 'express api'});
-});
-app.post ('/', (req, res) => {
-  res.send ('You can post to this endpoint.');
+const app = express();
+
+// // routes
+// app.get ('/', (req, res) => {
+//   res
+//     .status (200)
+//     .json ({mesage: 'Hello from the server.', app: 'express api'});
+// });
+// app.post ('/', (req, res) => {
+//   res.send ('You can post to this endpoint.');
+// });
+
+const tours = JSON.parse(
+	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+// TOURS ROUTES
+//get all tours route
+app.get('/api/v1/tours', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		results: tours.length,
+		data: {
+			tours,
+		},
+	});
 });
 
 // start server
 const port = 3000;
-app.listen (port, () => {
-  console.log (`App running on port ${port}...`);
+app.listen(port, () => {
+	console.log(`App running on port ${port}...`);
 });
