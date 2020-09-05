51500b82e09beced3cda59e7ed3085c750091a71
brianokanga <briannokanga@gmail.com>
2020-09-05 11:42:05 +0300

    added post for create new tour

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); //simple middleware
+
 // // routes
 // app.get ('/', (req, res) => {
 //   res
@@ -29,6 +31,25 @@ app.get('/api/v1/tours', (req, res) => {
 	});
 });
 
+// get all tours route
+app.post('/api/v1/tours', (req, res) => {
+	const newId = tours[tours.length - 1].id + 1;
+	const newTour = Object.assign({ id: newId }, req.body);
+
+	tours.push(newTour);
+	fs.writeFile(
+		`${__dirname}/dev-data/data/tours-simple.json`,
+		JSON.stringify(tours),
+		err => {
+			res.status(201).json({
+				status: 'success',
+				data: {
+					tour: newTour,
+				},
+			});
+		}
+	);
+});
 // start server
 const port = 3000;
 app.listen(port, () => {
