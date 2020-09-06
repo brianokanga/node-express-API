9519e3d2f269055bc9620ef8b9c11aa790b24825
brianokanga <briannokanga@gmail.com>
2020-09-06 06:48:09 +0300

    Added middleware to check if request body has name or price

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-const { route } = require('./routes/tourRoutes');
+const express = require ('express');
+const morgan = require ('morgan');
+const {route} = require ('./routes/tourRoutes');
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const app = express();
+const tourRouter = require ('./routes/tourRoutes');
+const userRouter = require ('./routes/userRoutes');
+const app = express ();
 
 // 1. MIDDLEWARES
 // The order of middleware determines how they are used
 
 // Third party logger middleware
-app.use(morgan('dev'));
+app.use (morgan ('dev'));
 
 // Simple middleware
-app.use(express.json());
+app.use (express.json ());
 
 // Custom middleware functionS
-app.use((req, res, next) => {
-	console.log(`Hello from the middleware!!`);
-	next();
+app.use ((req, res, next) => {
+  console.log (`Hello from the middleware!!`);
+  next ();
 });
 
-app.use((req, res, next) => {
-	req.requestTime = new Date().toString();
-	next();
+app.use ((req, res, next) => {
+  req.requestTime = new Date ().toString ();
+  next ();
 });
 
 // 2. ROUTES HANDLERS
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 // 3. ROUTES
 
 // Mounting the routers
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use ('/api/v1/tours', tourRouter);
+app.use ('/api/v1/users', userRouter);
 
 module.exports = app;
