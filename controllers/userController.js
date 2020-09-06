de608ba242cb5877e2ef539ab52433a5fc284e90
brianokanga <briannokanga@gmail.com>
2020-09-06 05:20:56 +0300

    Added controllers folder with user and tour controler inside and moved routes handlers there from app.js

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+// is) Routes handlers
+// Get all tours route handler
+exports.getAllUsers = (req, res) => {
+	res.status(500).json({
+		status: 'success',
+		message: 'This route is not yet defined!',
+	});
+};
+
+// Get User route handler
+exports.getUser = (req, res) => {
+	res.status(500).json({
+		status: 'success',
+		message: 'This route is not yet defined!',
+	});
+};
+
+// Create User route handler
+exports.createUser = (req, res) => {
+	res.status(500).json({
+		status: 'success',
+		message: 'This route is not yet defined!',
+	});
+};
+
+// Update user route handler
+exports.updateUser = (req, res) => {
+	res.status(500).json({
+		status: 'success',
+		message: 'This route is not yet defined!',
+	});
+};
+
+// Delete User route handler
+exports.deleteUser = (req, res) => {
+	res.status(500).json({
+		status: 'success',
+		message: 'This route is not yet defined!',
+	});
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,53 +1,20 @@
 const express = require('express');
+const userController = require('./../controllers/userController');
 
-// is) Routes handlers
-// Get all tours route handler
-const getAllUsers = (req, res) => {
-	res.status(500).json({
-		status: 'success',
-		message: 'This route is not yet defined!',
-	});
-};
-
-// Get User route handler
-const getUser = (req, res) => {
-	res.status(500).json({
-		status: 'success',
-		message: 'This route is not yet defined!',
-	});
-};
-
-// Create User route handler
-const createUser = (req, res) => {
-	res.status(500).json({
-		status: 'success',
-		message: 'This route is not yet defined!',
-	});
-};
-
-// Update user route handler
-const updateUser = (req, res) => {
-	res.status(500).json({
-		status: 'success',
-		message: 'This route is not yet defined!',
-	});
-};
-
-// Delete User route handler
-const deleteUser = (req, res) => {
-	res.status(500).json({
-		status: 'success',
-		message: 'This route is not yet defined!',
-	});
-};
-
-// ii) Routes definitions
+// i) Routes definitions
 const router = express.Router();
 
 //1. '/api/v1/users'
-router.route('/').get(getAllUsers).post(createUser);
+router
+	.route('/')
+	.get(userController.getAllUsers)
+	.post(userController.createUser);
 
 //2. '/api/v1/tours/:id'
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+router
+	.route('/:id')
+	.get(userController.getUser)
+	.patch(userController.updateUser)
+	.delete(userController.deleteUser);
 
 module.exports = router;
