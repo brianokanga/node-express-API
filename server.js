23fb4c8b5d17506c92fa1c79b57dece5cf7ba5f0
brianokanga <briannokanga@gmail.com>
2020-09-08 01:04:45 +0300

    Added mongodb and mongoose configurations

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-const dotnev = require('dotenv');
-dotnev.config({ path: './config.env' });
+const mongoose = require ('mongoose');
+const dotnev = require ('dotenv');
+const app = require ('./app');
+dotnev.config ({path: './config.env'});
 
-const app = require('./app');
+// mongoose database connection
+const DB = process.env.DATABASE.replace (
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect (DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then (con => {
+    console.log (con.connections);
+    console.log ('DB connection successful');
+  });
 
-// console.log(app.get('env'))
-// console.log(process.env);
+// const DB = process.env.DATABASE_LOCAL;
+// mongoose
+//   .connect (DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then (con => {
+//     console.log (con.connections);
+//     console.log ('DB connection successful');
+//   });
+
+console.log (app.get ('env'));
+console.log (process.env);
 
 // 1. START THE SERVER
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-	console.log(`App running on port ${port}...`);
+app.listen (port, () => {
+  console.log (`App running on port ${port}...`);
 });
