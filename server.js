49dfbeb10a97c02a9047083c3cca9cdcacdbe936
brianokanga <briannokanga@gmail.com>
2020-09-08 07:14:52 +0300

    Added mongodb and mongoose configurations

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const mongoose = require ('mongoose');
-const dotnev = require ('dotenv');
-const app = require ('./app');
-dotnev.config ({path: './config.env'});
+const mongoose = require('mongoose');
+const dotnev = require('dotenv');
+const app = require('./app');
+
+dotnev.config({
+  path: './config.env'
+});
 
 // DATABASE CONNECTIONS
 
@@ -11,48 +14,67 @@ dotnev.config ({path: './config.env'});
 //   .connect (DB, {
 //     useNewUrlParser: true,
 //     useCreateIndex: true,
-//     useFindAndModify: false,
-//   })
-//   .then (con => {
+//     useFindAen (con => {
 //     console.log (con.connections);
 //     console.log ('DB connection successful');
 //   });
 
 // Remote database connection
-const DB = process.env.DATABASE.replace (
+const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 mongoose
-  .connect (DB, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
+    useFindAndModify: false
   })
-  .then (() => console.log ('DB connection successful'));
+  .then(() => console.log('DB connection successful'))
+  .catch(err => {
+    console.log('ERROR', err);
+  });
 
 // Schema
-const tourSchema = new mongoose.Schema ({
+const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'], //validator
-    unique: true,
+    unique: true
   },
   rating: {
     type: Number,
-    default: 4.5,
+    default: 4.5
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price'],
-  },
+    required: [true, 'A tour must have a price']
+  }
 });
 
 // Tour model
-const Tour = mongoose.model ('Tour', tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
+
+// Documents (instance of the tour model)
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497
+});
+
+// save the document to the database
+// save returns a promise
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR', err);
+  });
 
 // 1. START THE SERVER
 const port = process.env.PORT || 3000;
-app.listen (port, () => {
-  console.log (`App running on port ${port}...`);
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
