b2ed32cf8b9e732ffa86c1b3ae8b0848106250d6
brianokanga <briannokanga@gmail.com>
2020-09-08 03:21:11 +0300

    Added tour schema and model

--- server.js
+++ server.js
@@ -3,22 +3,9 @@ const dotnev = require ('dotenv');
 const app = require ('./app');
 dotnev.config ({path: './config.env'});
 
-// mongoose database connection
-const DB = process.env.DATABASE.replace (
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-mongoose
-  .connect (DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then (con => {
-    console.log (con.connections);
-    console.log ('DB connection successful');
-  });
+// DATABASE CONNECTIONS
 
+// Local database
 // const DB = process.env.DATABASE_LOCAL;
 // mongoose
 //   .connect (DB, {
@@ -31,12 +18,41 @@ mongoose
 //     console.log ('DB connection successful');
 //   });
 
-console.log (app.get ('env'));
-console.log (process.env);
+// Remote database connection
+const DB = process.env.DATABASE.replace (
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect (DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then (() => console.log ('DB connection successful'));
+
+// Schema
+const tourSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], //validator
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Tour model
+const Tour = mongoose.model ('Tour', tourSchema);
 
 // 1. START THE SERVER
 const port = process.env.PORT || 3000;
-
 app.listen (port, () => {
   console.log (`App running on port ${port}...`);
 });
