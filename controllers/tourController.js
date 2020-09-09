3271c1d7c75037ce82c44cdcbea492044d77ab6e
brianokanga <briannokanga@gmail.com>
2020-09-09 03:55:28 +0300

    Added updateTour async function to query and update a tour from the database using mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -68,13 +68,25 @@ exports.createTour = async (req, res) => {
 };
 
 // Update a Tour route handler
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findOneAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      }
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // Delete a Tour route handler
