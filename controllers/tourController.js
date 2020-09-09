11f280ad6a120927bd7f89d32ace15842b21dc45
brianokanga <briannokanga@gmail.com>
2020-09-09 08:22:52 +0300

    Added basics of filtering(building and implimenting queries)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,7 +10,27 @@ const {
 // find() returns a promise
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = {
+      ...req.query
+    };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    console.log(req.query, queryObj);
+
+
+    // FILTERING
+    // BUILD A QUERY
+    //  method 1
+    const query = Tour.find(queryObj);
+
+    // method 2
+    // const query = Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+
+    // EXECUTE A QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
