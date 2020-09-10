785bceaa5e396c943f784845df884316f24f417c
brianokanga <briannokanga@gmail.com>
2020-09-10 05:56:24 +0300

    added aggretion pipelines examples for solving monthly plan

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,6 +13,8 @@ router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.ge
 // AGRREGATION route
 router.route('/tour-stats').get(tourController.getTourStats);
 
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 //1. '/api/v1/tours'
 router
   .route('/')
